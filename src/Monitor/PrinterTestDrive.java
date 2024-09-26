a3c4e94a00436d8c619bd5116c8185f0222c7430
Edgar Morales <edgarmorales@ciencias.unam.mx>
2024-09-26 13:56:16 -0600

    modificación

--- src/Monitor/PrinterTestDrive.java
+++ src/Monitor/PrinterTestDrive.java
@@ -5,7 +5,7 @@ import singleton.Printer;
 public class PrinterTestDrive {
     public static void main(String[] args) {
 
-        //Programar lo que te va a mostrar el monitor de las impresiones 
+        //Programar lo que te va a mostrar el monitor de las impresiones
         //Printer printer = new Printer();
     }
 }
